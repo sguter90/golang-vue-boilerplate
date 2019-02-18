775fb062fb59d5b4a2832d63cfbba43c09b49ae3
Christoph Mueller <christoph@flying-lama.com>
2019-02-18 18:36:43 +0100

    serve home page and static files

---
+++ internal/app/serve.go
@@ -0,0 +1,80 @@
+package app
+
+import (
+	"context"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+func (app *App) Serve(params []string) error {
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(app.appCtx)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	r.Get("/", home)
+	r.Get("/ping", ping)
+
+	// serve static files
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "assets")
+	r.Handle("/assets", http.FileServer(http.Dir(filesDir)))
+	FileServer(r, "/assets", http.Dir(filesDir))
+
+	port := strconv.Itoa(app.Config.ServePort)
+	err := http.ListenAndServe(":"+port, r)
+
+	return err
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "app.html", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
+}
+
+func (app *App) appCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "db", app.Db)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
